cf3616befec900aa6c93d634ed28a4bb82ec27eb
kevdez <kernan373@gmail.com>
2016-02-23 15:49:38 -0800

    Added Materialize and created a starter version

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,37 +1,8 @@
-$(function() {
-  $(".tile1").click(function() {
-    $(".tile1").addClass("largeTile", 1000, callback);
-  });
+(function($){
+  $(function(){
 
-  function callback() {
-    $(".tile1").click(function() {
-      $(".tile1").removeClass("largeTile", 1000, "easeInOutCubic", callback);
-    });
-  }
-});
-$(function() {
-  $(".tile2").click(function() {
-    $(".tile2").addClass("largeTile", 1000, callback);
-  });
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
 
-  function callback() {
-    $(".tile2").click(function() {
-      $(".tile2").removeClass("largeTile", 1000, callback);
-    });
-  }
-});
-$(function() {
-  $(".tile3").click(function() {
-    $(".tile3").addClass("largeTile", 700, "easeOutBounce", callback);
-  });
-
-  function callback() {
-    $(".tile3").click(function() {
-      $(".tile3").removeClass("largeTile", 700, "easeOutBounce", callback);
-    });
-  }
-});
-
-$(function() {
-  $("#draggable").draggable();
-})
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
